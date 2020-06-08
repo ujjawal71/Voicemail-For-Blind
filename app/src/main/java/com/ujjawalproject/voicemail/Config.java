0668fdfa930a3385864657cbbd9c9e879958f424
ujjawalkumar <Ujjawalkumar04@gmail.com>
2020-06-08 09:59:32 +0530

    Voicemail-For-Blind

---
+++ app/src/main/java/com/ujjawalproject/voicemail/Config.java
@@ -0,0 +1,8 @@
+package com.ujjawalproject.voicemail;
+
+public class Config {
+
+    public static String EMAIL ="krhimanshu42@gmail.com";  //gmail address
+    public static String PASSWORD ="Ram854334#"; //password
+
+}
